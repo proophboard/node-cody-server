405e4f09e76daa75b516b5824665d5b225722b5b
codeliner <kontakt@codeliner.ws>
2022-01-22 23:13:55 +0100

    Configure QA tools

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+
+const path = require('path');
+
+const ignorePatterns = [
+    '/node_modules/',
+    '__fixtures__',
+    '/packages/cody-types/lib',
+];
+
+module.exports = {
+    rootDir: path.resolve(__dirname),
+    verbose: true,
+    testURL: 'http://localhost/',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ignorePatterns,
+    coveragePathIgnorePatterns: ignorePatterns,
+    transform: {
+        '^.+\\.[jt]sx?$': 'babel-jest',
+    },
+};
